2e0fcc08b87ef360cdca1e11e3aeed399aed25d2
Marcin Sola <marcinsolaonline@gmail.com>
2020-04-30 20:01:55 +0200

    MovieController scaffold

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,21 +9,27 @@ class MovieController extends Controller
 {
     public function index()
     {
-        return Movie::all()->toJson();
+        return Movie::all()->load('genres')->toJson();
     }
 
     public function store(Request $request)
     {
-        //
+        $movie = Movie::create($request->all());
+
+        return response()->json($movie, 201);
     }
 
     public function update(Request $request, Movie $movie)
     {
-        //
+        $movie->update($request->all());
+
+        return response()->json($movie, 200);
     }
 
     public function destroy(Movie $movie)
     {
-        //
+        $movie->delete();
+
+        return response()->json(null, 204);
     }
 }
