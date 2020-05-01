56aafa0dfda694bf40fa70b12161e5003a1eb0b9
Marcin Sola <marcinsolaonline@gmail.com>
2020-05-01 13:39:42 +0200

    Remove cover from MovieFactory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Movie::class, function (Faker $faker) {
     return [
-        'title' => $faker->words(rand(0, 3), true),
+        'title' => $faker->words(rand(1, 3), true),
         'description' => $faker->paragraph,
         'country' => $faker->countryCode
     ];
