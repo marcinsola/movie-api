474a4e090a2606ed6a8d1642c24b9ccb3b73dae5
Marcin Sola <marcinsolaonline@gmail.com>
2020-05-01 13:39:17 +0200

    Add foreign keys in movies_genres migration

--- database/migrations/2020_05_01_111636_create_movies_genres_table.php
+++ database/migrations/2020_05_01_111636_create_movies_genres_table.php
@@ -13,11 +13,23 @@ class CreateMoviesGenresTable extends Migration
             $table->unsignedBigInteger('movie_id');
             $table->unsignedBigInteger('genre_id');
             $table->timestamps();
+
+            $table->foreign('movie_id')
+                ->references('id')
+                ->on('movies');
+
+            $table->foreign('genre_id')
+                ->references('id')
+                ->on('genres');
         });
     }
 
     public function down()
     {
+        Schema::table('movies_genres', function (Blueprint $table) {
+           $table->dropForeign(['genre_id', 'movie_id']);
+        });
+
         Schema::dropIfExists('movies_genres');
     }
 }
